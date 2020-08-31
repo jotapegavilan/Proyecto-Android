16d07a4065c040e5b306c581308a541635a4d9fd
Jotape <j.p.daniel.gavilan@gmail.com>
2020-08-31 12:45:41 -0400

    Segundo commit: se agregan widgets y se generan las referencias desde Java

--- app/src/main/java/com/gavilan/proyecto_androiod/MainActivity.java
+++ app/src/main/java/com/gavilan/proyecto_androiod/MainActivity.java
@@ -2,13 +2,30 @@ package com.gavilan.proyecto_androiod;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    // Creamos la variables que harán referencia a los widgets
+    private TextView txtTitulo;
+    private Button btnIniciar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Realizar las referencias
+        txtTitulo = findViewById(R.id.txtTitulo);
+        btnIniciar = findViewById(R.id.btnIniciar);
+
+
+
+    }
+
+    public void Saludar(){
+
     }
 }
